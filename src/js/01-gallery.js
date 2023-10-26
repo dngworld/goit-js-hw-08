20b564e4e2d5e9013223a471730dcd61774d0ffc
dngworld <bulochka166.coco@gmail.com>
2023-10-25 22:28:25 -0400

    js-8
    Vimeo плеєр + бібліотеки

---
+++ src/js/01-gallery.js
@@ -0,0 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+const container = document.querySelector('.gallery');
+container.insertAdjacentHTML('afterbegin', createMarkup(galleryItems));
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+  <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    )
+    .join('');
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  close: true,
+});
